685262a73181b17ca96b2c11f9389cd8c8fea3ef
Qihao Qian <2363813095@qq.com>
2026-01-26 14:43:15 -0800

    project points to boundary

--- grad_sdf/frame.py
+++ grad_sdf/frame.py
@@ -114,12 +114,41 @@ class DepthFrame(Frame):
     def get_valid_mask(self):
         return self.valid_mask
 
+    def _project_points_to_boundary(
+        self, bound_min: torch.Tensor, bound_max: torch.Tensor, points_out_of_bound: torch.Tensor
+    ):
+        origin = self.get_ref_translation().view(1, 3)  # (1, 3)
+
+        ray_dir = points_out_of_bound - origin
+
+        inv_dir = 1.0 / (ray_dir + 1e-8)
+        t_min_planes = (bound_min.view(1, 3) - origin) * inv_dir
+        t_max_planes = (bound_max.view(1, 3) - origin) * inv_dir
+
+        t_max_each_dim = torch.max(t_min_planes, t_max_planes)
+        t_exit_final = torch.min(t_max_each_dim, dim=-1)[0]
+
+        projected_points_world = origin + t_exit_final.view(-1, 1) * ray_dir
+
+        R_w2c = self.ref_pose[:3, :3].T
+        t_w2c = -R_w2c @ self.ref_pose[:3, 3]
+
+        projected_points_cam = projected_points_world @ R_w2c.T + t_w2c
+
+        return projected_points_cam
+
     def apply_bound(self, bound_min: torch.Tensor, bound_max: torch.Tensor):
         points = self.points @ self.ref_pose[:3, :3].T + self.ref_pose[:3, 3]
-        mask = points >= bound_min.view(1, 1, 3)
-        mask = mask & (points <= bound_max.view(1, 1, 3))
+
+        mask = (points >= bound_min.view(1, 1, 3)) & (points <= bound_max.view(1, 1, 3))
         mask = mask.all(dim=-1)
-        self.valid_mask = self.valid_mask & mask
+
+        points_out_of_bound = points[~mask]
+
+        if points_out_of_bound.shape[0] > 0:
+            new_points = self.project_points_to_boundary(bound_min, bound_max, points_out_of_bound)
+
+            self.depth[~mask] = new_points[:, 2]
 
     def sample_points(
         self,
